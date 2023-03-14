bc7868e045fd2a9d362ad5ebe9cacdecd827f457
Osman <osman.cakir1@outlook.com>
2023-03-14 13:10:03 +1100

    bug fixing

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,26 +1,30 @@
-import { InformationCircleIcon } from '@heroicons/react/outline'
-import { useEffect, useState } from 'react'
-import { baseUrl } from '../constants/movie'
-import { Movie } from '../typings'
-import { FaPlay } from 'react-icons/fa'
-import { modalState, movieState } from '../atoms/modalAtom'
-import { useRecoilState } from 'recoil'
-import Image from 'next/legacy/image'
+import Image from "next/legacy/image";
+import { useEffect, useState } from "react";
+import { baseUrl } from "../constants/movie";
+import { Movie } from "../typings";
+import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/solid";
+import { modalState, movieState } from "../atoms/modalAtom"
+import { useRecoilState} from "recoil";
 
 interface Props {
-  netflixOriginals: Movie[]
+  netflixOriginals: Movie[];
 }
 
 function Banner({ netflixOriginals }: Props) {
-  const [movie, setMovie] = useState<Movie | null>(null)
-  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [movie, setMovie] = useState<Movie | null>(null);
   const [showModal, setShowModal] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  
+
 
   useEffect(() => {
     setMovie(
       netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
-    )
-  }, [netflixOriginals])
+    );
+  }, [netflixOriginals]);
+
+  console.log(movie);
 
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
@@ -28,35 +32,34 @@ function Banner({ netflixOriginals }: Props) {
         <Image
           layout="fill"
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-          objectFit="cover"
-          alt="Image Description"
+          style={{ objectFit: 'cover' }}
+          alt="Movie Backdrop"
         />
       </div>
 
-      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+      <h1 className="text-2xl lg:text-7xl md:text-4xl font-bold">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl text-shadow-md">
         {movie?.overview}
       </p>
+
       <div className="flex space-x-3">
         <button className="bannerButton bg-white text-black">
           <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
           Play
         </button>
 
-        <button
-          className="bannerButton bg-[gray]/70"
-          onClick={() => {
-            setCurrentMovie(movie)
-            setShowModal(true)
-          }}
-        >
-          <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" /> More Info
+        <button className="bannerButton bg-[grey]/70"
+        onClick={() => {
+          setCurrentMovie(movie)
+          setShowModal(true)
+        }}>
+          More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />{" "}
         </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
